c06273cab4804a51d144a649827e1842c2a114b3
Martin Heidegger <martin.heidegger@gmail.com>
2019-02-26 04:04:31 +0900

    Added examples that can be run in the command line.

---
+++ example/Readme.md
@@ -0,0 +1,4 @@
+## Licenses
+
+- [./japan.jpg](https://commons.wikimedia.org/wiki/File:Sea_of_japan_in_ricci_world_map.jpg)
+- [./tibet.svg](https://commons.wikimedia.org/wiki/File:Flag_of_Tibet.svg)
---
+++ example/exec.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+FOLDER=$(dirname $0)
+
+# ↓ Replace the next line with:
+# ↓ npx mapslice
+"$FOLDER/../bin/mapslice" \
+  -f "$FOLDER/japan.jpg" \
+  --tileSize 128 \
+  --skipEmptyTiles \
+  --background "#00000000"
+
+"$FOLDER/../bin/mapslice" \
+  -f "$FOLDER/tibet.svg" \
+  --output "$FOLDER/tibet/{z}_{y}_{x}.png" \
+  --tileSize 256 \
+  --parallelLimit 20 \
+  --background "white"
