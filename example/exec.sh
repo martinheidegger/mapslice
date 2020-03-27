5a7f3a53b52612cb61337c0057c427a8f39849c8
Martin Heidegger <martin.heidegger@gmail.com>
2020-03-27 19:23:58 +0900

    fix(docs): Added npx doc example

--- example/exec.sh
+++ example/exec.sh
@@ -4,13 +4,13 @@ FOLDER=$(dirname $0)
 
 # ↓ Replace the next line with:
 # ↓ npx mapslice
-"$FOLDER/../bin/mapslice" \
+npx mapslice \
   -f "$FOLDER/japan.jpg" \
   --tileSize 128 \
   --skipEmptyTiles \
   --background "#00000000"
 
-"$FOLDER/../bin/mapslice" \
+npx mapslice \
   -f "$FOLDER/tibet.svg" \
   --output "$FOLDER/tibet/{z}_{y}_{x}.png" \
   --tileSize 256 \
