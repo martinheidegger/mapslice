3f343fde859119d0206d7acecad0a300c79ed483
Martin Heidegger <martin.heidegger@gmail.com>
2020-08-11 22:31:50 +0900

    chore: standard fix for example

--- example/example.js
+++ example/example.js
@@ -1,3 +1,4 @@
+/* eslint no-multi-spaces: "off" */
 // ↓ Replace the next line with:
 // ↓ const MapSlicer = require('mapslice')
 const MapSlicer = require('..')
@@ -23,6 +24,6 @@ const mapSlicer = new MapSlicer({
 mapSlicer.on('start', (files, options) => console.info(`Starting to process ${files} files.`))
 mapSlicer.on('error', err => console.error(err))
 mapSlicer.on('warning', err => console.warn(err))
-mapSlicer.on('progress', progress => console.info(`Progress: ${Math.round(progress*100)}%`))
-mapSlicer.on('end', () => console.info('Finished processing slices.') )
+mapSlicer.on('progress', progress => console.info(`Progress: ${Math.round(progress * 100)}%`))
+mapSlicer.on('end', () => console.info('Finished processing slices.'))
 mapSlicer.start()
